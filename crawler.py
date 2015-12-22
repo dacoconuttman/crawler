f32db4f05067b179d32c9135d4bbeb487bd6115b
Andrew Ksendzoff <aksendzoff@smu.edu>
2015-12-22 17:18:34 -0600

    Gets array of websites to visit

--- crawler.py
+++ crawler.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+import csv
+import HTMLParser
+
+filename = '9d3dfb739fc3-list+of+urls.csv'
+websites = []
+with open(filename, 'r') as f:
+    for line in f:
+        websites.append(line)
+        if 'str' in line:
+            break
+for site in websites:
+    print site
