b99c11048c76cddcb1f2034b2aa878f6e46c5d24
Andrew Ksendzoff <aksendzoff@smu.edu>
2015-12-22 20:27:52 -0600

    Started multithreading site finding, may scrap it

--- crawler.py
+++ crawler.py
@@ -1,12 +1,29 @@
 import csv
 import HTMLParser
+import Queue
+import threading
 
-filename = '9d3dfb739fc3-list+of+urls.csv'
-websites = []
-with open(filename, 'r') as f:
-    for line in f:
-        websites.append(line)
-        if 'str' in line:
-            break
-for site in websites:
-    print site
+def findPages(url):
+    print url
+    return
+
+def getWebsites(filename):
+    websites = []
+    with open(filename, 'r') as f:
+        for line in f:
+            print line
+            websites.append(line)
+            if 'str' in line:
+                break
+    return websites
+
+siteList = getWebsites('9d3dfb739fc3-list+of+urls.csv')
+
+threads = []
+
+print len(siteList)
+
+for site in range(len(siteList)):
+    t = threading.Thread(target=findPages, args=(site,))
+    threads.append(t)
+    t.start()
